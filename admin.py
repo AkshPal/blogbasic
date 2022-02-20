e81f882ae667a4239edd35cabcd2e7c90ff08fd6
AkshPal <96865578+AkshPal@users.noreply.github.com>
2022-02-20 22:43:14 +0530

    Revert "Aksh pal blogapp"

--- admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from blog.models import blogpost, blogpostForm, Comments, profile
-
-# Register your models here.
-admin.site.register(profile)
-admin.site.register(blogpost)
-admin.site.register(Comments)
